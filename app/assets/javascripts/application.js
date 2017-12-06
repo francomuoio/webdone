d3fed49578086c56e8db7806e87e6bf40e629c2b
Florine Lequette <florine.lequette@gmail.com>
2017-12-06 15:31:51 +0100

    integration theme en cours

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,10 @@
+//= require jquery
 //= require rails-ujs
+//= require bootstrap-sprockets
+//= require vendor/theme/app.min.e05f769f
+//= require vendor/theme/plugins-init
+//= require vendor/theme/switchery-settings
 //= require_tree .
+
+
+console.log('salut la tortue')
