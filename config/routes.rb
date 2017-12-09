258c2a81cbb964ac4fcef4b340d2a1b639788efa
francomuoio <franco.59171@gmail.com>
2017-12-09 16:54:15 +0100

    ajout 404 500

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'errors/not_found'
+
+  get 'errors/internal_server_error'
+
   resources :develloppeur_profiles, only: :show
 
   resources :projets, except: :destroy do
@@ -16,4 +20,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   ActiveAdmin.routes(self)
+
+  match "/404", :to => "errors#not_found", :via => :all
+  match "/500", :to => "errors#internal_server_error", :via => :all
 end
