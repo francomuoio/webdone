ebf674c710f00a47df87456a9066d3faf34b46e8
francomuoio <franco.59171@gmail.com>
2017-12-11 17:56:54 +0100

    ajout index issues done en cours avant pull florine

--- app/controllers/dones_controller.rb
+++ app/controllers/dones_controller.rb
@@ -3,24 +3,28 @@ class DonesController < ApplicationController
   require 'open-uri'
 
   def index
-    @projet = Projet.find(params[:projet_id])
-    @dev = @projet.develloppeur_profile
-    #data = Github::Client::Issues.new oauth_token: current_user.develloppeur_profile.github_token, repo: @projet.repository_url
+    # @projet = Projet.find(params[:projet_id])
+    # @dev = @projet.develloppeur_profile
+    # #data = Github::Client::Issues.new oauth_token: current_user.develloppeur_profile.github_token, repo: @projet.repository_url
 
-    issues = Github::Client::Issues.new oauth_token: @dev.github_token
-    data = issues.list #'francomuoio', 'github', state: 'open'
-    @arr = []
-    # tes issues ou t'es assigné
-    gh_username = @dev.github_username
-    url = "https://api.github.com/repos/#{gh_username}/#{@projet.title.downcase}"
-    data.body.each do |aa|
-      # issue = {
-      #   comments_url: messages(clean_url(aa["comments_url"])),
-      #   repository_url: messages(clean_url(aa["repository_url"])),
-      #   labels: messages(clean_url(aa["labels_url"])),
-      #   comments: messages(clean_url(aa["comments_url"]))
-      #  }
-      @arr << aa.title if aa["repository_url"] == url
-    end
+    # issues = Github::Client::Issues.new oauth_token: @dev.github_token
+    # data = issues.list #'francomuoio', 'github', state: 'open'
+    # @arr = []
+    # # tes issues ou t'es assigné
+    # gh_username = @dev.github_username
+    # url = "https://api.github.com/repos/#{gh_username}/#{@projet.title.downcase}"
+    # data.body.each do |aa|
+    #   # issue = {
+    #   #   comments_url: messages(clean_url(aa["comments_url"])),
+    #   #   repository_url: messages(clean_url(aa["repository_url"])),
+    #   #   labels: messages(clean_url(aa["labels_url"])),
+    #   #   comments: messages(clean_url(aa["comments_url"]))
+    #   #  }
+    #   @arr << aa.title if aa["repository_url"] == url
+    @projet = Projet.find(params[:projet_id])
+    service = GithubIssuesService.new(current_user.develloppeur_profile.github_token)
+    @issues = service.get_issues(@projet)
+    @comments = service.get_comments(@projet)
+
   end
 end
