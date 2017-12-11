2a532c6382b777624262d59d2a653dc60549c75e
Matthieu Seynaeve <matthieu.seynaeve@free.fr>
2017-12-11 18:08:10 +0100

    WIP Messages

--- app/controllers/projets_controller.rb
+++ app/controllers/projets_controller.rb
@@ -15,6 +15,7 @@ class ProjetsController < ApplicationController
     if current_user.role == "developpeur"
       dev = DevelloppeurProfile.find_by user_id: current_user.id
       @projets = Projet.where(develloppeur_profile_id: dev.id)
+      @service = GithubIssuesService.new(current_user.develloppeur_profile.github_token)
     else
       client = ClientProfile.find_by user_id: current_user.id
       @projets = Projet.where(client_profile_id: client.id)
