d6cc13001dcb5e8e31a0ba8478157d6760708661
francomuoio <franco.59171@gmail.com>
2017-12-12 19:22:56 +0100

    a

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,14 +2,14 @@ class MessagesController < ApplicationController
 
   def index
     @projet = Projet.find(params[:projet_id])
-    service = GithubIssuesService.new(current_user.develloppeur_profile.github_token)
+    service = GithubIssuesService.new(@projet.develloppeur_profile.github_token)
     @issues = service.get_issues(@projet)
     @comments = service.get_comments(@projet)
   end
 
   def create
-    projet = Projet.find(params[:projet_id])
-    service = GithubIssuesService.new(current_user.develloppeur_profile.github_token)
+    @projet = Projet.find(params[:projet_id])
+    service = GithubIssuesService.new(@projet.develloppeur_profile.github_token)
     service.create_comment(projet, params[:issue_number], params[:body])
     redirect_to projet_messages_path(projet)
   end
