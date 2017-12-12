9e6b6582dab503d969c091e435afdedd3b431303
Yann IRBAH <yann.irbah@gmail.com>
2017-12-12 14:39:16 +0100

    Cleans the theme integration

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,9 +1,3 @@
-//= require jquery
 //= require rails-ujs
-//= require bootstrap-sprockets
 //= require cable
 //= require active_admin
-
-
-
-console.log('salut la tortue')
---
+++ app/assets/javascripts/theme.js
@@ -0,0 +1,6 @@
+//= require vendor/lib.min
+//= require vendor/helpers
+//= require vendor/layoutControl
+// require vendor/rightSidebar
+// require vendor/sidebar
+// require vendor/main
