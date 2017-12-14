7152248c71a5f08e5a86dcd4eee37181b514088e
Florine Lequette <florine.lequette@gmail.com>
2017-12-14 17:06:21 +0100

    en cours

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,27 +6,40 @@ class UsersController < ApplicationController
     # @projet = Projet.find(params[:repository_url])
     email = params[:user][:email]
     pwd = params[:user][:password]
-    @user = User.new
-    @user.email = email
-    @user.password = 'ChangezVotreMdp'
-    if @user.save
-      UserMailer.userinvitation(@user).deliver_now
-      # creation de profil
-      clientprofile = ClientProfile.create!(
-        first_name: 'changezvotreprenom',
-        last_name: 'changezvotrenom',
-        phone_number: '0000000000',
-        remote_photo_url: 'http://via.placeholder.com/150x150',
-        user_id: @user.id
-      )
-      projet.client_profile_id = clientprofile.id
-      projet.save!
-      # rattachement du profil au projet
-      # ajout de la photo
-      # save du profil
-      redirect_to projets_path
+
+    client_in = User.find_by email: email
+    if client_in.nil?
+      @user = User.new
+      @user.email = email
+      @user.password = 'ChangezVotreMdp'
+      if @user.save
+        # creation de profil
+        clientprofile = ClientProfile.create!(
+          first_name: 'changezvotreprenom',
+          last_name: 'changezvotrenom',
+          phone_number: '0000000000',
+          remote_photo_url: 'http://via.placeholder.com/150x150',
+          user_id: @user.id
+        )
+        projet.client_profile_id = clientprofile.id
+        projet.save!
+        # rattachement du profil au projet
+        # ajout de la photo
+        # save du profil
+        redirect_to projets_path
+      else
+        redirect_to projets_path(projet)
+      end
+
     else
-      redirect_to projets_path(projet)
+      cli = ClientProfile.find_by user_id: client_in.id
+      projet.client_profile_id = cli.id
+      if projet.save!
+        redirect_to projets_path
+      else
+        redirect_to projets_path(projet)
+      end
     end
   end
 end
+
